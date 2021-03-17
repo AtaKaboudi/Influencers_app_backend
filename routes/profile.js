08c4dc65420a73bd5e20f5c2a27b2b687b082f36
AtaKaboudi <ata.kaboudi@gmail.com>
2021-03-18 00:11:40 +0100

    Profile_mgm : DELETE MODIFY GET

--- routes/profile.js
+++ routes/profile.js
@@ -21,18 +21,57 @@ db.connect((err)=>{
 })
 
 
-router.get('',(req,res)=>{
-    db.query('SELECT * FROM '+process.env.DATABASE_USER_TABLE+' where user_id = ? ',req.body.user_id,(err,resu)=>{
+router.get('/getAll',(req,res)=>{
+    db.query('SELECT * FROM '+process.env.DATABASE_USER_TABLE+' WHERE user_id = ? ',req.body.user_id,(err,resu)=>{
        if(err) return res.send(err)
        if(resu.length != 1) return res.send("CHECK ID")
         res.json(resu);
     } )
+    console.log('[Profile] GET ALL '+ req.body.user_id)
 
 })
+
+/*
+    getAttribute : 
+    body : { 
+        user_id : 1 , 
+        attribute : first_name
+    }
+*/
+
+router.get('/getAttribute/',(req,res)=>{
+
+    db.query('SELECT '+req.body.attribute+' FROM '+process.env.DATABASE_USER_TABLE+' WHERE user_id = ? ',req.body.user_id,(err,resu)=>{
+        if(err) return res.send(err)
+        if(resu.length != 1) return res.send("CHECK ID")
+         res.json(resu);
+     } )
+
+     console.log('[Profile] GET '+ req.body.attribute +' '+ req.body.user_id)
+
+})
+
+
+ /* modify  : 
+    
+post  https : ..... /modify/firstname 
+    Req body : {user_id : 1=2 , new_value : "ata"}
+
+ */
 router.post('/modify/:attribute',(req,res)=>{
     let attributeToChange = req.params.attribute ;
    console.log(req.body.attributeToChange); 
-   // db.query('UPDATE '+process.env.DATABASE_USER_TABLE+' SET '+ attributeToChange +' :' + req.body.attributeToChange)
+    db.query('UPDATE '+process.env.DATABASE_USER_TABLE+' SET '+ attributeToChange +'= "' + req.body.new_value +'" WHERE user_id = '+ req.body.user_id);
+ 
+    console.log('[Profile] UPDATE '+ req.params.attribute+' '+ req.body.user_id)
+
+})
+
+router.post('/delete',(req,res)=>{
+
+    db.query('DELETE '+process.env.DATABASE_USER_TABLE+' WHERE user_id = '+ req.body.user_id);
+    
+    console.log('[Profile] DELETE '+ req.body.user_id)
 })
 
 
